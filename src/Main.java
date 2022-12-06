d0a9a8e3848d36319002edb21217e312d160446d
Ahmad Akorede <56229105+cordova-001@users.noreply.github.com>
2022-12-06 03:29:40 -0800

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello world!")
+        int age = 24;
+        float bet = 12.90F;
+        String firstName = "Ahmad Akorede";
+        char name = 'A';
+        String 
+        System.out.println(firstName);
+    }
+}
