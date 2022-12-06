63f8027b7b34fbc4d3a1999eb3a7a59235ede6ac
cordova-001 <aabiola610@gmail.com>
2022-12-06 13:33:38 +0100

    Additional Lessons on string added

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,15 @@ public class Main {
         float bet = 12.90F;
         String firstName = "Ahmad Akorede";
         char name = 'A';
-        String 
-        System.out.println(firstName);
+        String names = "Ahmad" + "Akorede";
+//        names.toLowerCase(); to print lower case
+//        names.toUpperCase(); to print all character in Upper case
+//        names.endsWith(); to check if the string ends with a particular
+//        names.length(); to check the length of the characters in string
+//        names.replace("A", "B"), is used to replace a character with another character
+//        names.trim(); this is used to remove white spaces in the string
+//        Escaping sequence with \ and \n
+                System.out.println(names.replace("A", "B"));
+//        System.out.println(names);
     }
 }
